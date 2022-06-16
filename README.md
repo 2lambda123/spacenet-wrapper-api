78412e0aaaea0b7e65f20e068f9a1e10817d32b8
Paul Grogan <paul@ptgrogan.com>
2022-06-16 00:41:27 -0400

    push initial development

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# spacenet-wrapper-api
-A thin wrapper API to expose SpaceNet Java analysis capabilities
+# SpaceNet Wrapper API
+
+This project provides a thin Python-based wrapper API that exposes SpaceNet
+Java analysis capabilities as a RESTful HTTP service.
+
+## Installation
+
+1. Install the minimal required Python dependencies with the shell command:
+```
+pip install -r requirements.txt
+```
+
+2. Create a `.env` file in this directory, and specify the location of your
+SpaceNet Java Archive (JAR) file. For example (on Mac/Linux):
+```
+SPACENET_PATH = /your/path/to/spacenet-2.5.1464-jar-with-dependencies.jar
+```
+or (on Windows):
+```
+SPACENET_PATH = C:\your\path\to\spacenet-2.5.1464-jar-with-dependencies.jar
+```
+
+## Usage
+
+Start the application with the shell command:
+```
+uvicorn app.main:app --reload
+```
+the API service is available at <http://localhost:8000/docs>.
+
+Upload scenario files (.json or .xml) to view demands analysis results.
+
+## Contact
+
+Paul T. Grogan  <paul@ptgrogan.com>
+
+## License
+
+Copyright 2022 Paul T. Grogan
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ app/main.py
@@ -0,0 +1,112 @@
+import aiofiles
+import os
+import subprocess
+from dotenv import load_dotenv
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from tempfile import TemporaryDirectory
+from fastapi.responses import RedirectResponse
+
+from .demands_analysis import RawDemandsAnalysis, AggregatedDemandsAnalysis
+
+# load .env file
+load_dotenv()
+
+# create application
+app = FastAPI()
+
+# load the spacenet jar path from environment variable
+spacenet_path = os.getenv("SPACENET_PATH")
+if not os.path.exists(spacenet_path):
+    raise (RuntimeError(f"SpaceNet JAR not found at: {spacenet_path}"))
+
+# load the spacenet execution timeout from environment variable (default: 10 s)
+spacenet_timeout = os.getenv("SPACENET_TIMEOUT", 10)
+
+
+@app.get("/", include_in_schema=False)
+async def docs_redirect():
+    return RedirectResponse(url="/docs")
+
+
+@app.post("/demands-raw", tags=["demands"], response_model=RawDemandsAnalysis)
+async def analyze_raw_demands(scenario_file: UploadFile = File(...)):
+    """
+    Analyze raw demands for a scenario. Raw demands describe the time and
+    location of resources demanded by elements and/or missions.
+    \f
+    :param scenario_file: User input (scenario).
+    """
+    # create a temp directory for working files
+    with TemporaryDirectory() as tempdir:
+        scenario_path = os.path.join(tempdir, scenario_file.filename)
+        results_path = os.path.join(tempdir, "results.json")
+        # write scenario file to temporary directory
+        async with aiofiles.open(scenario_path, "wb") as new_scenario_file:
+            content = await scenario_file.read()
+            await new_scenario_file.write(content)
+        # call spacenet headless script
+        try:
+            subprocess.check_output(
+                [
+                    "java",
+                    "-jar",
+                    spacenet_path,
+                    "-h",
+                    "demands-raw",
+                    "-i",
+                    scenario_path,
+                    "-o",
+                    results_path,
+                ],
+                stderr=subprocess.STDOUT,
+                shell=True,
+                universal_newlines=True,
+                timeout=spacenet_timeout,
+            )
+        except subprocess.CalledProcessError as e:
+            raise HTTPException(status_code=422, detail=e.output)
+        except subprocess.TimeoutExpired as e:
+            raise HTTPException(
+                status_code=422, detail=f"Timeout exceeded ({spacenet_timeout} s)"
+            )
+        # read analysis outputs
+        async with aiofiles.open(results_path, "r") as results_file:
+            results = await results_file.read()
+    return RawDemandsAnalysis.parse_raw(results)
+
+
+@app.post("/demands-agg", tags=["demands"], response_model=AggregatedDemandsAnalysis)
+async def analyze_aggregated_demands(scenario_file: UploadFile = File(...)):
+    """
+    Analyze aggregated demands for a scenario. Aggregated demands group
+    resources demanded by elements and/or missions to a time and node
+    (supply node) or a start/end time and edge (supply edge).
+    \f
+    :param scenario_file: User input (scenario).
+    """
+    # create a temp directory for working files
+    with TemporaryDirectory() as tempdir:
+        scenario_path = os.path.join(tempdir, scenario_file.filename)
+        results_path = os.path.join(tempdir, "results.json")
+        # write scenario file to temporary directory
+        async with aiofiles.open(scenario_path, "wb") as new_scenario_file:
+            content = await scenario_file.read()
+            await new_scenario_file.write(content)
+        # call spacenet headless script
+        subprocess.call(
+            [
+                "java",
+                "-jar",
+                spacenet_path,
+                "-h",
+                "demands-agg",
+                "-i",
+                scenario_path,
+                "-o",
+                results_path,
+            ]
+        )
+        # read analysis outputs
+        async with aiofiles.open(results_path, "r") as results_file:
+            results = await results_file.read()
+    return AggregatedDemandsAnalysis.parse_raw(results)
